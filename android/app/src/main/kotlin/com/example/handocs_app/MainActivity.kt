8370826667760fe476f3561b2f51ca100ac2a50b
cicero.axa <cicero.axa@gmail.com>
2024-02-26 22:53:45 -0300

    Tela main construida.

---
+++ android/app/src/main/kotlin/com/example/handocs_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.handocs_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/helpers/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class HDColor {
+  static Color buttonBronzeLight = const Color.fromRGBO(212, 166, 52, 1.0);
+  static Color buttonBronze = const Color.fromRGBO(140, 100, 10, 1.0);
+  static Color buttonText = const Color.fromRGBO(255, 255, 255, 1.0);
+}
