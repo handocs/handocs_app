b4bd232593f9157119c789c3d5e9cf355f795132
almir_bf <almir_bf@hotmail.com>
2024-07-08 23:08:49 -0300

    itemcategory_page.dart

---
+++ lib/app/(public)/itemcategory_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItemcategoryPage extends StatefulWidget {
+  const ItemcategoryPage({super.key});
+
+  @override
+  State<ItemcategoryPage> createState() => _ItemcategoryPageState();
+}
+
+class _ItemcategoryPageState extends State<ItemcategoryPage> {
+  //final _itemcategoriaModel = Routefly.query.arguments as ItemcategoriaModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+    }
+}
